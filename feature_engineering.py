1f205a66f2af53c4bf9d054f5aae268665eb72aa
Nanda Kishor Panda <nkpanda97@gmail.com>
2021-12-12 18:51:00 +0100

    Update feature_engineering.py
    Skeleton code of pre-data processing

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,1 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 18:35:16 2021
 
+@author: nkpanda97
+
+This file contains all the functions used for pre-data processing
+"""
+def plot_feature_properties(Input):
+    """
+    This function should do te following things:
+        1. Print number of features, instances, and number of missing data per features
+            A plot can be used to show number of missing features/ feature
+        2. Plot the mean, variance and standard deviation of all features
+        
+        3. Plot the chi2 test score for all features and  
+            sort it. Other appropiate tests can also be used.
+        
+        4. Plot corelation of various features and also show the features which have constant data
+
+    Parameters
+    ----------
+    Input : Pandas dataframe
+        Povided dataset
+
+    Returns
+    -------
+    None.
+
+    """
+def feature_reduction(Input):
+    """
+    
+
+    Parameters
+    ----------
+    Input : Pandas dataframe
+        Povided dataset
+
+    Returns
+    -------
+    reduced_input1 : pandas dataframe or np array
+        Data with reduced instance by deleting missing data
+
+   reduced_input2 : pandas dataframe or np array
+        Data with reduced feature  according to statistcal tool (eg: ch2 test)
+    
+   reduced_input3: pandas dataframe or np array
+         Data with reduced feature  according to domain knowledge
+    """
+    
+    return reduced_input1, reduced_input2, reduced_input3
+
+def add_feature(Input):
+    """
+    
+
+    Parameters
+    ----------
+    Input : Pandas dataframe
+        Povided dataset
+
+    Returns
+    -------
+    new_data : Tpandas dataframe or np array
+        Data with new features combined
+
+    """
+    
+    
+    
+    return new_data
+    
